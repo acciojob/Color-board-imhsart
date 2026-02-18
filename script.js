a53cc417208f7f58dc1b1c187782ae77ac204f17
Nitin Rana <nitinrana2519@gmail.com>
2026-02-18 19:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let container = document.querySelector('.container')
+for(let i=1;i<=800;i++){
+	let box = document.createElement('div')
+	box.className = 'square';
+	container.append(box);
+}
+let squares = document.querySelectorAll('.square')
+
+for(let i of squares){
+	i.addEventListener('mouseenter', (e)=> {
+		let rc = () => parseInt(Math.random()*255)
+		let color = `rgb(${rc()},${rc()},${rc()})`
+		e.target.style.backgroundColor = color;
+	})
+	i.addEventListener('mouseleave', (e)=> {
+			e.target.style.backgroundColor = '#232023';
+			e.target.style.transition = '1s';
+	})
+}
